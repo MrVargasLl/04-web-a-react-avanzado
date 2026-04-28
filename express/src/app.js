39ee4cc44c2bac7b83664458a54158fbb5852080
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2026-04-27 21:25:20 -0400

    Creacion y uso de Vbles de entorno

---
+++ express/src/app.js
@@ -0,0 +1,18 @@
+
+
+//Manejo de variables de entorno con Node.js
+//node--env-file=.env app.js
+/* "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node src/app.js",
+    "dev": "nodemon --env-file=.env src/app.js"
+    },
+ */
+
+ import { config } from 'dotenv'
+config() 
+
+//Variables de entorno
+console.log(process.env.PORT)
+console.log(process.env.HELLO)
+
