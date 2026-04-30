4c4d9fb426869d33522edb831e05c725f2867d7b
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2026-04-29 22:35:54 -0400

    Formulario Peliculas creado en Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { ChatBot } from './assets/components/ChatBot'
+import { FormularioPeliculas } from './assets/components/FormularioPeliculas'
 import { ChatProvider } from './assets/context/ChatContext'
 
 export const App = () => {
   return (
     <ChatProvider>
-      <ChatBot />
+      {/* <ChatBot /> */}
+      <FormularioPeliculas />
     </ChatProvider>
   )
 }
